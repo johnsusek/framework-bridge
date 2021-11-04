7da275b8a922351ab6e5a3ee94ce52c662fa2aba
John Susek <john@johnsolo.net>
2021-11-04 17:31:41 -0500

    Move subclass logic

--- Sources/FrameworkBridge/JSContext+Bridged.swift
+++ Sources/FrameworkBridge/JSContext+Bridged.swift
@@ -1,61 +1,5 @@
 import JavaScriptCore
 
-let subclassed = [
-  "AVPlayerView",
-  "IKCameraDeviceView",
-  "IKDeviceBrowserView",
-  "IKFilterBrowserView",
-  "IKFilterUIView",
-  "IKImageView",
-  "IKScannerDeviceView",
-  "NSBox",
-  "NSBrowser",
-  "NSButton",
-  "NSClipView",
-  "NSCollectionView",
-  "NSColorWell",
-  "NSComboBox",
-  "NSDatePicker",
-  "NSGridView",
-  "NSImageView",
-  "NSLevelIndicator",
-  "NSMatrix",
-  "NSOutlineView",
-  "NSPathControl",
-  "NSPopUpButton",
-  "NSPredicateEditor",
-  "NSProgressIndicator",
-  "NSRuleEditor",
-  "NSRulerView",
-  "NSScroller",
-  "NSScrollView",
-  "NSScrubber",
-  "NSScrubberArrangedView",
-  "NSScrubberImageItemView",
-  "NSScrubberTextItemView",
-  "NSSearchField",
-  "NSSecureTextField",
-  "NSSegmentedControl",
-  "NSSlider",
-  "NSSplitView",
-  "NSStackView",
-  "NSStatusBarButton",
-  "NSStepper",
-  "NSSwitch",
-  "NSTableCellView",
-  "NSTableHeaderView",
-  "NSTableRowView",
-  "NSTableView",
-  "NSTabView",
-  "NSText",
-  "NSTextField",
-  "NSTextView",
-  "NSTokenField",
-  "NSVisualEffectView",
-  "QLPreviewView",
-  "QuartzFilterView"
-]
-
 func classOrProtoFromString(_ name: String) -> Any? {
   let cls: AnyClass? = NSClassFromString(name)
   if cls != nil { return cls }
@@ -71,16 +15,6 @@ public extension JSContext {
     let classType = classOrProtoFromString(classOrProto)
     let className = classOrProto
     var registered = false
-
-    if subclassed.contains(className) {
-      let subclassName = String(className.dropFirst(2))
-
-      if let subClassType = classOrProtoFromString(subclassName) {
-        self.setObject(subClassType, forKeyedSubscript: subclassName as (NSCopying & NSObjectProtocol)?)
-        print("✔ Registered \(String(describing: subClassType)) as \(subclassName)")
-        registered = true
-      }
-    }
 
     if (classType != nil) {
       self.setObject(classType!, forKeyedSubscript: className as (NSCopying & NSObjectProtocol)?)

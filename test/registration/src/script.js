b875931dbd17577b5e200bb521dbcbbfc4b9387b
John Susek <john@johnsolo.net>
2021-11-16 03:08:50 -0600

    Add NSData

--- test/registration/src/script.js
+++ test/registration/src/script.js
@@ -29,6 +29,7 @@ const classesToRegister = {
     "NSCondition",
     "NSCountCommand",
     "NSCreateCommand",
+    "NSData",
     "NSDataDetector",
     "NSDecimalNumberHandler",
     "NSDeleteCommand",
